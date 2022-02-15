0b2f74d4aa22461a64f95b9ca319d252036c4711
Duc Vu <minhducnael00@gmail.com>
2022-02-15 00:02:08 -0500

    Postwidget + graphql API

--- pages/index.js
+++ pages/index.js
@@ -18,12 +18,12 @@ export default function Home({posts}) {
           ))}
         </div>
 
-        <div className='lg:cold-span-4 col-span-1'>
-        <div className='lg:sticky relative top-8'>
-          <PostWidget/>
-          <Categories/>
+        <div className='lg:col-span-4 col-span-1'>
+          <div className='lg:sticky relative top-8'>
+            <PostWidget/>
+            <Categories/>
+          </div>
         </div>
-      </div>
       </div>
 
       
